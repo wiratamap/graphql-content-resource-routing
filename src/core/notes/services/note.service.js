ae5570687946f17676c5776ea6c40e1f009f9a20
Wiratama Paramasatya <Wiratama.Paramasatya@btpn.com>
2019-05-24 14:47:00 +0700

    Able to create note via service layer

--- src/core/notes/services/note.service.js
+++ src/core/notes/services/note.service.js
@@ -12,6 +12,18 @@ const getRequest = async (authorization) => {
   });
 };
 
+const postRequest = async (authorization, newNote) => {
+  const endpoint = CONSTANT.NOTES_URL;
+
+  return fetch(endpoint, {
+    method: 'POST',
+    headers: {
+      Authorization: authorization,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newNote),
+  });
+};
 
 const fetchNotes = async (args, context) => {
   const { authorization } = { ...context.headers };
@@ -23,6 +35,19 @@ const fetchNotes = async (args, context) => {
   return result.json();
 };
 
+const createNote = async (args, context) => {
+  const { authorization } = { ...context.headers };
+
+  const { newNote } = { ...args };
+
+  const result = await postRequest(authorization, newNote);
+
+  if (result.status !== 200) throw new Error(result.statusText);
+
+  return result.json();
+};
+
 module.exports = {
   fetchNotes,
+  createNote,
 };
--- test/core/notes/services/note.service.test.js
+++ test/core/notes/services/note.service.test.js
@@ -1,15 +1,16 @@
 const nock = require('nock');
 const { expect } = require('chai');
-const { fetchNotes } = require('../../../../src/core/notes/services/note.service');
+const { fetchNotes, createNote } = require('../../../../src/core/notes/services/note.service');
 
 describe('note.service', () => {
   const context = {
     headers: {
-      Authorization: 'Bearer my-token',
+      authorization: 'Bearer my-token',
     },
   };
 
   const successResult = [{ title: 'this is title', content: 'this is content' }];
+  const createdNote = { title: 'this is title', content: 'this is content' };
 
   afterEach(() => {
     nock.cleanAll();
@@ -18,8 +19,8 @@ describe('note.service', () => {
   describe('fetchAllNotes', () => {
     it('should return notes when successfully retrieve data from the endpoint', async () => {
       nock('http://localhost:3000', {
-        reqheders: {
-          Authorization: context.headers.Authorization,
+        reqheaders: {
+          Authorization: context.headers.authorization,
         },
       }).get('/notes').reply(200, successResult);
 
@@ -32,8 +33,8 @@ describe('note.service', () => {
       const response = { status: 401, statusText: 'Unauthorized' };
 
       nock('http://localhost:3000', {
-        reqheders: {
-          Authorization: context.headers.Authorization,
+        reqheaders: {
+          Authorization: context.headers.authorization,
         },
       }).get('/notes').reply(401, response);
 
@@ -41,6 +42,38 @@ describe('note.service', () => {
         await fetchNotes(null, context);
       } catch (error) {
         expect(error.message).to.eql(response.statusText);
+      }
+    });
+  });
+
+  describe('createNote', () => {
+    it('should return created note when successfully create the note', async () => {
+      const args = { newNote: { title: 'my new note', content: 'this is the content' } };
+      nock('http://localhost:3000', {
+        reqheaders: {
+          authorization: context.headers.authorization,
+        },
+      }).post('/notes', args.newNote).reply(200, createdNote);
+
+      const result = await createNote(args, context);
+
+      expect(result).to.eql(createdNote);
+    });
+
+    it('should throw error when something went wrong', async () => {
+      const args = { newNote: { title: 'my new note', content: 'this is the content' } };
+      const response = { status: 500, statusText: 'Internal Server Error' };
+
+      nock('http://localhost:3000', {
+        reqheaders: {
+          authorization: context.headers.authorization,
+        },
+      }).post('/notes', args.newNote).reply(500, response);
+
+      try {
+        await createNote(args, context);
+      } catch (error) {
+        expect(error.message).to.equal(response.statusText);
       }
     });
   });
